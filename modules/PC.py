7cc3787a1b357a5bfb6e002838e636b8a16bb612
ImShallo <shallogamer@gmail.com>
2024-05-04 18:01:37 +0200

    Refactor PC module to use Queue instead of Coda

--- modules/PC.py
+++ modules/PC.py
@@ -3,12 +3,15 @@
 # Project: undefined
 # ----------------- # 
 
-from .Coda import Coda
+from .Queue import Queue
 
 class PC:
-    def __init__(self, id) -> None:
+    def __init__(self, id):
         self.id: str = id
-        self.buffer: Coda = []
+        self.buffer: Queue = []
 
     def __str__(self) -> str:
         return f"PC {self.id} Buffer: {self.buffer}"
+
+    def receiveFrame(self, frame):
+        self.buffer.push(frame)
--- modules/__init__.py
+++ modules/__init__.py
@@ -1,6 +1,6 @@
 # modules/__init__.py
 
 from .PC import PC
-from .Coda import Coda
+from .Queue import Queue
 from .Frame import Frame
 from .Switch import Switch
