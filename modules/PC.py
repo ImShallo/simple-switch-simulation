3a9df6ace735f90525eb2d109fe2008634f03f48
ImShallo <shallogamer@gmail.com>
2024-05-04 21:43:33 +0200

    Remove unused attributes

--- modules/PC.py
+++ modules/PC.py
@@ -8,10 +8,6 @@ from .Queue import Queue
 class PC:
     def __init__(self, id):
         self.id: str = id
-        self.buffer: Queue = []
 
     def __str__(self) -> str:
         return f"PC {self.id} Buffer: {self.buffer}"
-
-    def receiveFrame(self, frame):
-        self.buffer.append(frame)
