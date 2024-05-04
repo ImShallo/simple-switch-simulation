fc76c97048e6d8ef6a0656a64c21d87d2b7a6c25
ImShallo <shallogamer@gmail.com>
2024-05-04 18:31:23 +0200

    Refactor to buffer and receiveFrame func

--- modules/Switch.py
+++ modules/Switch.py
@@ -4,20 +4,35 @@
 # ----------------- # 
 
 from .PC import PC
-from .Coda import Coda
+from .Queue import Queue
 from .Frame import Frame
 
 class Switch:
-    def __init__(self, *args, buffer = "A") -> None:
-        self.listaPC: list[PC] = list(args)
-        self.buffer: Coda = buffer
+    def __init__(self, *args, bufferSize):
+        self.listPCs: list[PC] = list(args)
+        self._buffer: Queue[Frame] = Queue(int(bufferSize))
+        self.bufferSizeHistory: list[int] = []
 
     def __str__(self) -> str:
         string: str = "--- Switch ---\n"
-        for pc in self.listaPC:
+        for pc in self.listPCs:
             string += f" - {pc}\n" 
-        string += f"Buffer: {self.buffer}"
+        string += f"Buffer size: {self._buffer.lenght()}\n"
+        string += f"Buffer: "
+
+        # * Possibile miglioramento per la formattazione #1 https://www.tldraw.com/r/fV9OGlrdxCvJ9nwrYMCEK?v=-4075,-1556,2187,1038&p=e4VFSbeoSGqjqzGHUQUVB
+        for frame in self._buffer:
+            string += f"\n\t| {frame}"
+
         return string
 
-    def sendFrame(self, frame: Frame) -> None:
-        self.buffer.push(frame)
+    def receiveFrame(self, frame: Frame):
+        self._buffer.append(frame)
+
+    def sendFrame(self):
+        self.bufferSizeHistory.append(self._buffer.lenght())
+
+        while not self._buffer.is_empty():
+            # frame.destination.receiveFrame(frame)
+            print("Rimosso frame dal buffer")
+            self._buffer.pop()
