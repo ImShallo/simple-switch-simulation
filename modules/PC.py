847d2f89f241d143f825208076d1086ce3279bbf
ImShallo <shallogamer@gmail.com>
2024-05-05 20:49:39 +0200

    Add some methods and fix circular import

--- modules/PC.py
+++ modules/PC.py
@@ -3,11 +3,24 @@
 # Project: undefined
 # ----------------- # 
 
+from __future__ import annotations
+from typing import TYPE_CHECKING
 from .Queue import Queue
+
+if TYPE_CHECKING: # solve circular import
+    from .Frame import Frame
 
 class PC:
     def __init__(self, id):
         self.id: str = id
+        self._buffer: Queue[Frame] = Queue()
 
     def __str__(self) -> str:
+        
         return f"PC {self.id} Buffer: {self.buffer}"
+    
+    def receiveFrame(self, frame: Frame):
+        self._buffer.append(frame)
+
+    def totalFramesReceived(self) -> int:
+        return self._buffer.lenght()
